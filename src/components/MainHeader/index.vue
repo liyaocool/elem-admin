45c5170d9908cd8dc55ddbe2ff274df93c5e530c
liyaocool <crazyliyao@163.com>
2019-02-13 18:10:17 +0800

    引入element-ui 主页面

---
+++ src/components/MainHeader/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="MainHeader">
+    <el-row :gutter="24">
+      <el-col :span="4">
+        <div class="grid-content bg-purple">
+          <h1 class="top_logo"><i class="fa fa-bar-chart"></i> Element Admine</h1>
+        </div>
+      </el-col>
+      <el-col :span="3" :offset="17">
+        <div class="grid-content bg-purple">
+          <el-button>登录</el-button>
+          <el-button>注册</el-button>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainHeader"
+};
+</script>
+
+<style scoped lang="scss">
+.MainHeader {
+  height: 100%;
+  background-color: rgb(35, 118, 212);
+  color: #fff;
+
+  .el-row{
+    line-height: 60px;
+
+    .top_logo{
+      font-size: 20px;
+      font-weight: 600;
+      text-align: center;
+    }
+  }
+}
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-header>
+        <MainHeader/>
+      </el-header>
+      <el-container>
+        <el-aside>
+          <NavBar/>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import MainHeader from "@/components/MainHeader";
+import NavBar from "@/components/NavBar";
+
+export default {
+  name: "home",
+  components: {
+    MainHeader,
+    NavBar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  .el-header {
+    padding: 0;
+  }
+  .el-aside {
+    width: 250px !important;
+  }
+}
+</style>
+
